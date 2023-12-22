d21f1e3dfdab7e8672c506431d19fd69734face6
Leo Golubyev <leo.golubyev@gmail.com>
2023-12-22 09:55:53 +0100

    aa

---
+++ lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Líneas de idioma de restablecimiento de contraseña
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma son las líneas predeterminadas que coinciden con las razones
+    | que da el administrador de contraseñas cuando falla un intento de actualización de contraseña,
+    | como por ejemplo por un token inválido o por una contraseña nueva inválida.
+    |
+    */
+
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Hemos enviado por correo electrónico el enlace para restablecer tu contraseña!',
+    'throttled' => 'Por favor, espera antes de volver a intentarlo.',
+    'token' => 'Este token para restablecer la contraseña no es válido.',
+    'user' => 'No se puede encontrar un usuario con esa dirección de correo electrónico.',
+
+];
